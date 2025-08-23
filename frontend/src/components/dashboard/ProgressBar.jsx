e8feae411d6014981cd7b54016343e32ff6fe8d3
EkutyEbu <ekuty2001@gmail.com>
2025-08-23 15:55:32 +0100

    fix to dashboard heading

--- frontend/src/components/dashboard/ProgressBar.jsx
+++ frontend/src/components/dashboard/ProgressBar.jsx
@@ -1,22 +1,3 @@
-// import React from "react";
-
-// function ProgressBar({ score }) {
-//   return (
-//     <div className="w-full bg-gray-200 rounded-full h-4">
-//       <div
-//         className="bg-eco-green items-center h-4 rounded-full transition-all duration-500"
-//         style={{ width: `${score}%` }}
-//       >
-//         <span className="text-white text-xs font-bold pl-2">
-//           {score}% Green
-//         </span>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ProgressBar;
-
 import React from "react";
 
 function ProgressBar({ score }) {
